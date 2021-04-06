8998fe3b18d4708b62467377ae4e8c99c8a137f1
Henrik Hall <henrik.hall@sonymobile.com>
2021-04-06 23:07:08 +0200

    wip

--- src/DynamoDbClient.ts
+++ src/DynamoDbClient.ts
@@ -1,27 +1,232 @@
-import {buildConditionExpression, ConditionSet} from 'dynamodb-expressions'
+import {buildConditionExpression, buildUpdateExpression, ConditionSet, UpdateAttributes} from 'dynamodb-expressions'
+import {
+  DeleteCommand,
+  DynamoDBDocumentClient,
+  GetCommand, GetCommandInput,
+  PutCommand,
+  QueryCommand,
+  ScanCommand, TransactWriteCommand, TransactWriteCommandInput, UpdateCommand, UpdateCommandInput,
+} from '@aws-sdk/lib-dynamodb';
+import {TransactionCanceledException} from '@aws-sdk/client-dynamodb';
 
-declare interface DynamoDBDocument {
-  get(params: {Key: Record<string, unknown>}): Promise<unknown>;
-  query(params: any): Promise<any>;
+export interface GetParams<T, K extends KeyAttributes<T>, P extends keyof T> {
+  key: KeyValue<T, K>;
+  projection?: Array<P>;
 }
+
+export interface ScanResult<T, P extends keyof T = keyof T> {
+  items: Array<Pick<T, P>>,
+  nextPageToken?: string
+}
+
+export interface ScanParams<T, N extends string, P extends keyof T = keyof T, F extends keyof T = keyof T> {
+  indexName?: N;
+  pageToken?: string;
+  limit?: number;
+  projection?: Array<P>;
+  filterConditions?: ConditionSet<Pick<T, F>>;
+}
+
+export interface QueryParams<T, N extends string, P extends keyof T, I extends keyof T> extends ScanParams<T, N, P, Exclude<keyof T, I>> {
+  keyConditions: ConditionSet<T, I>;
+  ascending?: boolean;
+}
+
+export interface PutParams<T, A extends keyof T> {
+  item: Pick<T, A>;
+  conditions?: ConditionSet<T>;
+}
+
+export interface DeleteParams<T, K extends KeyAttributes<T>> {
+  key: KeyValue<T, K>;
+  conditions?: ConditionSet<T>;
+}
+
+export interface UpdateParams<T, K extends KeyAttributes<T>> {
+  key: KeyValue<T, K>;
+  attributes: UpdateAttributes<T>;
+  conditions?: ConditionSet<T>;
+}
+
+function createGetCommand<T, K extends KeyAttributes<T>, P extends keyof T>(
+    model: DynamoDBModel<T>,
+    params: GetParams<T, K, P>
+): GetCommandInput {
+  const {key, projection} = params;
+  return {
+    TableName: model.tableName,
+    Key: key,
+    ProjectionExpression: projection?.join(', '),
+  };
+}
+
+function createScanCommand<T, N extends string, P extends keyof T, F extends keyof T>(
+    model: DynamoDBModel<T>,
+    params: ScanParams<T, N, P, F>,
+) {
+  const attributes = {};
+  const {indexName, filterConditions, pageToken, limit, projection} = params;
+
+  return {
+    TableName: model.tableName,
+    IndexName: indexName,
+    FilterExpression: filterConditions && buildConditionExpression(filterConditions, attributes),
+    ExclusiveStartKey: parsePageToken(pageToken),
+    Limit: limit,
+    ProjectionExpression: projection?.join(', '),
+    ...attributes,
+  };
+}
+
+function createQueryCommand<T, N extends string, P extends keyof T, I extends keyof T>(
+    model: DynamoDBModel<T>,
+    params: QueryParams<T, N, P, I>
+) {
+  const attributes = {};
+  const {indexName, keyConditions, filterConditions, projection, limit, ascending, pageToken} = params;
+
+  return {
+    TableName: model.tableName,
+    IndexName: indexName,
+    KeyConditionExpression: buildConditionExpression(keyConditions, attributes),
+    FilterExpression: filterConditions && buildConditionExpression(filterConditions, attributes),
+    ExclusiveStartKey: parsePageToken(pageToken),
+    Limit: limit,
+    ProjectionExpression: projection?.join(', '),
+    ScanIndexForward: ascending,
+    ...attributes,
+  };
+}
+
+function createPutRequest<T, A extends keyof T>(
+    model: DynamoDBModel<T>,
+    params: PutParams<T, A>
+) {
+  const attributes = {};
+  const {item, conditions} = params;
+
+  Object.assign(item, model.params.creator?.(item));
+  //addTimes(item, true);
+
+  return {
+    TableName: model.tableName,
+    Item: item,
+    ConditionExpression: conditions && buildConditionExpression(conditions, attributes),
+    ...attributes,
+  };
+}
+
+function createUpdateRequest<T, K extends KeyAttributes<T>>(
+    model: DynamoDBModel<T, K>,
+    params: UpdateParams<T, K>
+) {
+  const attr = {};
+  const {key, attributes, conditions} = params;
+  Object.assign(attributes, model.params.updater?.(attributes));
+  //addTimes(attributes, false);
+
+  return {
+    TableName: model.tableName,
+    Key: key,
+    ReturnValues: 'ALL_NEW',
+    UpdateExpression: buildUpdateExpression(attributes, attr),
+    ConditionExpression: conditions && buildConditionExpression(conditions, attr),
+    ...attr,
+  };
+}
+
+function createDeleteRequest<T, K extends KeyAttributes<T>>(
+    model: DynamoDBModel<T, K, any>,
+    params: DeleteParams<T, K>
+) {
+  const attr = {};
+  const {key, conditions} = params;
+
+  return {
+    TableName: model.tableName,
+    Key: key,
+    ReturnValues: 'NONE',
+    ConditionExpression: conditions && buildConditionExpression(conditions, attr),
+    ...attr,
+  };
+}
+
+// declare interface DynamoDBDocument {
+//   get(params: {Key: Record<string, unknown>}): Promise<unknown>;
+//   query(params: any): Promise<any>;
+// }
 
 //type DynamoDBDocument = any; // TODO aws sdk v2 and v3 support?
 
 abstract class DynamoDbWrapper {
-  constructor(readonly dc: DynamoDBDocument) {
+  constructor(readonly dc: DynamoDBDocumentClient) {
+  }
+
+  protected async send(command: any): Promise<any> {
+    try {
+      return await this.dc.send(command);
+    } catch (err) {
+      throw err;
+    }
   }
 }
 
 class DynamoDbTransaction extends DynamoDbWrapper {
-  update() {
+  private readonly items: NonNullable<TransactWriteCommandInput['TransactItems']> = [];
+
+  put<T, K extends KeyAttributes<T>, B>(
+      model: DynamoDBModel<T, K>,
+      ...paramsList: Array<PutParams<T, Exclude<keyof T, keyof B>>>
+  ): DynamoDbTransaction {
+    this.items.push(...paramsList.map(params => ({Put: createPutRequest(model, params)})));
+
+    return this;
+  }
+
+  update<T, K extends KeyAttributes<T>>(
+      model: DynamoDBModel<T, K>,
+      ...paramsList: Array<UpdateParams<T, K>>
+  ): DynamoDbTransaction {
+    this.items.push(...paramsList.map(params => ({Update: createUpdateRequest(model, params)})));
+
+    return this;
+  }
+
+  delete<T, K extends KeyAttributes<T>>(
+      model: DynamoDBModel<T, K>,
+      ...paramsList: Array<DeleteParams<T, K>>
+  ): DynamoDbTransaction {
+    this.items.push(...paramsList.map(params => ({Delete: createDeleteRequest(model, params)})));
+
     return this;
   }
 
   async commit(): Promise<void> {
+    await this.send(new TransactWriteCommand({
+      TransactItems: this.items
+    }));
   }
+
+  static isConditionalCheckFailed(err: any) {
+    return err.name === 'TransactionCanceledException' && (err as TransactionCanceledException)
+        .CancellationReasons?.some(r => r.Code === 'ConditionalCheckFailed');
+  }
+
 }
 
 class DynamoDbBatchGetStatement extends DynamoDbWrapper {
+  get<T, K extends KeyAttributes<T>>(
+      model: DynamoDBModel<T, K>,
+      keys: Array<GetParams<T, K>>
+  ): DynamoDbBatchGetStatement {
+    const {Responses: {[this.tableName]: items = []} = {}} = await this.ddb.batchGet({
+      RequestItems: {
+        [this.tableName]: {Keys: keys}
+      },
+    });
+
+    return {items: items as T[]};
+  }
 }
 
 class DynamoDbBatchWriteStatement extends DynamoDbWrapper {
@@ -39,17 +244,23 @@ type KeyIndices<T> = Record<string, KeyAttributes<T>>;
 type AttributeType = 'string' | 'number' | 'binary';
 type AttributeSchema<T> = Record<string, AttributeType>;
 
-type ModelParams<T, K extends KeyAttributes<T>, I extends KeyIndices<T>> = {
+//type BaseItemCreator<T, B> = <U = Omit<T, keyof B>> (item: U) => B;
+//type BaseItemCreator<T, B, U = Omit<T, keyof B>> = (item: U) => B;
+//type BaseItemCreator<T, B, U> = (item: U) => B;
+
+type ModelParams<T, K extends KeyAttributes<T>, I extends KeyIndices<T>, B> = {
   keyAttributes?: K;
-  indices: I
+  indices: I;
+  creator?: (item: any) => B;
+  updater?: (item: any) => Partial<B>;
 };
 
-class DynamoDBModelBuilder<T, K extends KeyAttributes<T> = never, I extends KeyIndices<T> = {}> extends DynamoDbWrapper {
-  private readonly params: ModelParams<T, K, I> = {
+class DynamoDBModelBuilder<T, K extends KeyAttributes<T> = never, I extends KeyIndices<T> = {}, B = {}> extends DynamoDbWrapper {
+  private readonly params: ModelParams<T, K, I, B> = {
     indices: {} as I
   };
 
-  constructor(dc: DynamoDBDocument, readonly tableName: string) {
+  constructor(dc: DynamoDBDocumentClient, readonly tableName: string) {
     super(dc);
   }
 
@@ -60,26 +271,52 @@ class DynamoDBModelBuilder<T, K extends KeyAttributes<T> = never, I extends KeyI
     return builder;
   }
 
-  withIndex<N extends string, _I extends KeyAttributes<T>>(name: N, ...indexAttributes: _I): DynamoDBModelBuilder<T,  K, I & Record<N, _I>> {
-    const builder = this as unknown as DynamoDBModelBuilder<T, K, I & Record<N, _I>>;
+  withIndex<N extends string, IK extends KeyAttributes<T>>(name: N, ...indexAttributes: IK): DynamoDBModelBuilder<T, K, I & Record<N, IK>> {
+    const builder = this as unknown as DynamoDBModelBuilder<T, K, I & Record<N, IK>>;
 
     builder.params.indices[name] = indexAttributes as any;
 
     return builder;
   }
 
+  // withIndex2<_I extends KeyIndices<T>>(name: keyof _I, ...indexAttributes: _I[keyof _I]): DynamoDBModelBuilder<T, K, I & _I> {
+  //   const builder = this as unknown as DynamoDBModelBuilder<T, K, I & _I>;
+  //
+  //   builder.params.indices[name] = indexAttributes as any;
+  //
+  //   return builder;
+  // }
+
+  //withCreator<_B, U = Omit<T, keyof _B>>(creator: (item: U) => _B) {
+  withCreator<_B>(creator: (item: any) => _B) {
+    const builder = this as unknown as DynamoDBModelBuilder<T, K, I, _B>;
+
+    builder.params.creator = creator;
+
+    return builder;
+  }
+
+  withUpdater(updater: (item: Omit<T, keyof B>) => Partial<B>) {
+    const builder = this;
+
+    builder.params.updater = updater;
+
+    return builder;
+  }
+
+
   withSchema(schema: AttributeSchema<T>) {
     // Optional but required for model.createTable()
     // TODO
   }
 
-  build(): DynamoDBModel<T, K, I> {
-    return new DynamoDBModel<T, K, I>(this.dc, this.tableName, this.params);
+  build(): DynamoDBModel<T, K, I, B> {
+    return new DynamoDBModel(this.dc, this.tableName, this.params);
   }
 }
 
 export class DynamoDbClient {
-  constructor(readonly dc: DynamoDBDocument) {
+  constructor(readonly dc: DynamoDBDocumentClient) {
   }
 
   model<T>(tableName: string): DynamoDBModelBuilder<T> {
@@ -99,19 +336,6 @@ export class DynamoDbClient {
   }
 }
 
-export interface ScanParams<T, N extends string, P extends keyof T = keyof T, F extends keyof T = keyof T> {
-  indexName?: N;
-  pageToken?: string;
-  limit?: number;
-  projection?: Array<P>;
-  filterConditions?: ConditionSet<Pick<T, F>>;
-}
-
-export interface QueryParams<T, N extends string, P extends keyof T, I extends keyof T> extends ScanParams<T, N, P, Exclude<keyof T, I>> {
-  keyConditions: ConditionSet<Pick<T, I>>;
-  ascending?: boolean;
-}
-
 export type Item = Record<string, any>;
 
 function parsePageToken(pageToken: string | undefined): Item | undefined {
@@ -122,39 +346,70 @@ function formatPageToken(lastKey: Item | undefined) {
   return lastKey && Buffer.from(JSON.stringify(lastKey)).toString('base64');
 }
 
-class DynamoDBModel<T, K extends KeyAttributes<T>, I extends KeyIndices<T>> extends DynamoDbWrapper {
-  constructor(readonly dc: DynamoDBDocument, readonly tableName: string, private readonly params: ModelParams<T, K, I>) {
+type WriteItem<T, B> = Omit<T, keyof B>;
+type ReadItem<T, B> = WriteItem<T, B> & B;
+
+export class DynamoDBModel<T, K extends KeyAttributes<T> = any, I extends KeyIndices<T> = any, B = any> extends DynamoDbWrapper {
+  constructor(readonly dc: DynamoDBDocumentClient, readonly tableName: string, readonly params: ModelParams<T, K, I, B>) {
     super(dc);
   }
 
-  async get(key: KeyValue<T, K>): Promise<T | undefined> {
-    return await this.dc.get({Key: key}) as T | undefined;
+  async get<P extends keyof T>(
+      params: GetParams<T, K, P>
+  ): Promise<ReadItem<T, B> | undefined> {
+    const {Item: item} = await this.send(new GetCommand(createGetCommand(this, params)));
+
+    return item as ReadItem<T, B> | undefined;
   }
 
-  // async query<N extends keyof I>(indexName: keyof I, keyConditions: ConditionSet<KeyValue<T, I[N]>>) {
-  //
-  // }
-  async query<N extends keyof I & string, P extends keyof T>(params: QueryParams<T, N, P, Key<T, I[N]>>) {
-    const attributes = {};
-    const {indexName, keyConditions, filterConditions, projection, limit, ascending, pageToken} = params;
-
-    const {Items: items = [], LastEvaluatedKey: lastKey} = await this.dc.query({
-      TableName: this.tableName,
-      IndexName: indexName,
-      KeyConditionExpression: buildConditionExpression(keyConditions, attributes),
-      FilterExpression: filterConditions && buildConditionExpression(filterConditions, attributes),
-      ExclusiveStartKey: parsePageToken(pageToken),
-      Limit: limit,
-      ProjectionExpression: projection?.join(', '),
-      ScanIndexForward: ascending,
-      ...attributes,
-    });
+  async scan<N extends string, P extends keyof T>(
+      params: ScanParams<T, N, P> = {}
+  ): Promise<ScanResult<T, P>> {
+    const {Items: items = [], LastEvaluatedKey: lastKey} = await this.send(new ScanCommand(createScanCommand(this, params)));
 
     return {
       items: items as Array<Pick<T, P>>,
       nextPageToken: formatPageToken(lastKey),
     };
   }
+
+  async query<N extends string, P extends keyof T>(
+      params: QueryParams<T, N, P, Key<T, N extends keyof I ? I[N] : K>>
+  ): Promise<ScanResult<T, P>> {
+    const {Items: items = [], LastEvaluatedKey: lastKey} = await this.send(new QueryCommand(createQueryCommand(this, params)));
+
+    return {
+      items: items as Array<Pick<T, P>>,
+      nextPageToken: formatPageToken(lastKey),
+    };
+  }
+
+  async put(
+      params: PutParams<T, Exclude<keyof T, keyof B>>
+  ): Promise<{item: T}> {
+    await this.send(new PutCommand(createPutRequest(this, params)));
+
+    return {
+      item: params.item as T
+    };
+  }
+
+  async update(
+      params: UpdateParams<T, K>
+  ): Promise<{item: T}> {
+    const {Attributes: item} = await this.send(new UpdateCommand(createUpdateRequest(this, params)));
+
+    return {
+      item: item as T
+    };
+  }
+
+  async delete(
+      params: DeleteParams<T, K>
+  ): Promise<void> {
+    await this.send(new DeleteCommand(createDeleteRequest(this, params)));
+  }
+
 }
 
 type Person = {
@@ -165,13 +420,44 @@ type Person = {
 };
 
 async function foo() {
+
   const model = new DynamoDbClient(null as any).model<Person>('persons')
       .withKey('id', 'email')
       .withIndex('foo-index', 'name', 'age')
       .withIndex('bar-index', 'age')
+      .withIndex('foo', 'email')
+      .withCreator(x => ({id: x.email.reverse() as string, createdTime: new Date().toJSON(), modifiedTime: new Date().toJSON()}))
+      .withUpdater(x => ({modifiedTime: new Date().toJSON()}))
       .build();
 
-  await model.get({id: 'a', email: 'foo'})
+  const item = await model.get({key: {id: 'a', email: 'foo'}});
 
+  await model.query({indexName: 'foo-index', keyConditions: {name: '', age: 45}})
   await model.query({indexName: 'bar-index', keyConditions: {age: 45}})
+  await model.query({keyConditions: {id: 'a'}})
+
+  //const x: PutParams<Person, Exclude<keyof Person, never>> = {item: {name: 'foo', id: 'foo', email: 'foo'}};
+  //const x: PutParams<Person, keyof Record<string, never>> = {item: {}};
+
+  await model.put({item: {name: 'FOO', email: 'foo@bar.com', age:45}})
 }
+
+class Something<T> {
+  constructor(readonly value: T) {
+  }
+}
+
+type Foo<T, K extends keyof any = keyof any> = {
+  [P in Extract<keyof T, K>]?: Something<T[P]>;
+} & {
+  [P in Exclude<K, keyof T>]?: Something<unknown>;
+};
+
+
+
+const f1: Foo<Person, string> = {name: new Something('45'), age: new Something(45), foo: new Something(true), id: new Something('44')}
+f1[42] = new Something(true);
+const l = f1[3]
+const f2: Foo<Person, 'name' | 'age'> = {name: new Something('a'), age: new Something(45)}
+const f3: Foo<Person, 'foo'> = {foo: new Something(false)}
+const f4: Foo<Person, keyof Person> = {name: new Something('false')}
--- src/Model.ts
+++ src/Model.ts
@@ -1,6 +1,6 @@
 type Couple<T> = [T, T];
 
-type Key<T> = keyof T | Couple<keyof T>;
+type Key<T> = keyof T & string | Couple<keyof T & string>;
 
 type KeyValue<T, K extends Key<T>> = K extends Couple<keyof T> ?
     [T[K[0]], T[K[1]]] :
@@ -16,39 +16,41 @@ type KeyCondition<T, K extends Key<T>> = K extends Couple<keyof T> ?
         T[K] :
         never;
 
-type ItemGenerator<T, G> = (item: Readonly<Omit<T, keyof G>>) => G;
+type Diff<T, U> = Omit<T, keyof U>;
+
+type ItemGenerator<T, G> = <U = Diff<T, G>> (item: Readonly<U>) => G;
 type ItemUpdater<T, G, U extends Partial<T & G>> = (item: Readonly<Partial<T>>) => U;
 
-class ModelBuilder<T, K extends Key<T> = never, I extends Key<T> = never, G = never> {
-  private _key: K;
-  private _indices: Array<I> = [];
-  private _generators: Array<ItemGenerator<T, G>> = [];
-  private _updaters: Array<ItemUpdater<T, G, unknown>> = [];
+class ModelBuilder<T, K extends Key<T> = never, I extends Key<T> = never, G = {}> {
+  private _key: K | undefined;
+  private _indices: Array<{attributes: I, name: string}> = [];
+  private _generators: Array<ItemGenerator<T, any>> = [];
+  private _updaters: Array<ItemUpdater<T, G, any>> = [];
 
   constructor(private readonly tableName: string) {}
 
   key<K2 extends Key<T>>(key: K2): ModelBuilder<T, K2, I, G> {
-    const builder = this as ModelBuilder<T, K2, I, G>;
+    const builder = this as unknown as ModelBuilder<T, K2, I, G>;
     builder._key = key;
 
     return builder;
   }
 
-  index<I2 extends Key<T>>(index: I2): ModelBuilder<T, K, I | I2, G> {
+  index<I2 extends Key<T>>(attributes: I2, name: string): ModelBuilder<T, K, I | I2, G> {
     const builder = this as ModelBuilder<T, K, I | I2, G>;
-    builder._indices.push(index);
+    builder._indices.push({attributes, name});
 
     return builder;
   }
 
   generator<G2>(generator: ItemGenerator<T, G2>): ModelBuilder<T, K, I, G & G2> {
-    const builder = this as ModelBuilder<T, K, I, G & G2>;
+    const builder = this as unknown as ModelBuilder<T, K, I, G & G2>;
     builder._generators.push(generator);
 
     return builder;
   }
 
-  updator<U extends Partial<T & G>>(updater: ItemUpdater<T, G, U>): ModelBuilder<T, K, I, G> {
+  updater<U extends Partial<T & G>>(updater: ItemUpdater<T, G, U>): ModelBuilder<T, K, I, G> {
     const builder = this;
     builder._updaters.push(updater);
 
@@ -56,54 +58,49 @@ class ModelBuilder<T, K extends Key<T> = never, I extends Key<T> = never, G = ne
   }
 
   build(): Model<T, K, I, G> {
+    if (!this._key) {
+      throw new Error(`Cannot build model without specified key`);
+    }
     return new Model<T, K, I, G>(this.tableName, this._key, this._indices, this._generators, this._updaters);
   }
 }
 
 export default class Model<T, K extends Key<T>, I extends Key<T> = never, G = never> {
-  readonly key: K;
-  readonly indices: Array<I>;
-  private readonly generators: Array<ItemGenerator<T, G>>;
-  private readonly updaters: Array<ItemUpdater<T, G, unknown>>;
-
   constructor(
       readonly tableName: string,
       readonly key: K,
-      readonly indices: Array<I>,
+      readonly indices: Array<{attributes: I, name: string}>,
       private readonly generators: Array<ItemGenerator<T, G>>,
-      private readonly updaters: Array<ItemUpdater<T, G, unknown>>) {}
+      private readonly updaters: Array<ItemUpdater<T, G, any>>) {
+  }
 
-  public get(key: KeyValue<T, K>): Promise<T> {
+  public get(key: KeyValue<T, K>): Promise<T & G> {
     return null as any;
   }
 
-  public add(item: Omit<T, G>): Promise<T> {
+  public add(item: Diff<T, G>): Promise<T & G> {
     return null as any;
   }
 
-  public put(item: Omit<T, G>): Promise<T> {
+  public put(item: Diff<T, G>): Promise<T & G> {
     return null as any;
   }
 
-  public delete(key: K): Promise<T> {
+  public delete(key: KeyValue<T, K>): Promise<T & G> {
     return null as any;
   }
 
-  public update(key: KeyValue<T, K>, attributes: Partial<Omit<T, G>>, conditions?: []): Promise<T> {
+  public update(key: KeyValue<T, K>, attributes: Partial<Diff<T, G>>, conditions?: []): Promise<T & G> {
     return null as any;
   }
 
-  public query<I2 extends I>(index: I2, keyCondition: KeyCondition<T, I2>): Promise<T> {
+  public query<I2 extends I>(index: I2, keyCondition: KeyCondition<T, I2>): Promise<{items: Array<T & G>}> {
     return null as any;
   }
-
-
-
 
   static define<T>(tableName: string): ModelBuilder<T> {
     return new ModelBuilder<T>(tableName);
   }
-
 }
 
 type Person = {
@@ -113,27 +110,47 @@ type Person = {
   age?: number;
 };
 
-type Type<T> = T;
-const TYPE_TOKEN = Symbol();
+async function foo() {
+  const m = Model.define<Person>('persons')
+      .key('id')
+      .index(['name', 'age'], 'index-nameAge')
+      .generator((item) => ({id: '42'}))
+      .generator((item) => ({createdTime: new Date().toJSON(), modifiedTime: new Date().toJSON()}))
+      .updater((item) => ({modifiedTime: new Date().toJSON()}))
+      .build();
 
-function Type<T>(): T {
-  return TYPE_TOKEN as T;
+  const person = await m.add({name: 'John', email: 'john@doe.com'});
+  await m.update('42', {});
 }
 
-// const foo2 = token<Person>;
-// const foo3: <T> () => T;
-// type X = typeof foo3;
+// let x: {a: string} & Record<never, never>
 //
-// const foo = <T>42;
-
-class Foo<A, B> {
-  constructor(a: Type<A>, b: B) {}
-  get(): A {
-    return 42 as A;
-  }
-}
-
-new Foo(Type<Person>(), 42).get().name
-
-
-Model.define<Person>('persons').key<Key<Person>>(['id', 'age'])
+//
+//
+// interface Type<T> {
+// }
+//
+// //type Type<T> = (x: T) => TypeToken<T>;
+// const TYPE_TOKEN = Symbol() as any;
+//
+// function Type<T>(): Type<T> {
+//   return TYPE_TOKEN;
+// }
+//
+// // const foo2 = token<Person>;
+// // const foo3: <T> () => T;
+// // type X = typeof foo3;
+// //
+// // const foo = <T>42;
+//
+// class Foo<A, B> {
+//   constructor(a: Type<A>, b: B) {}
+//   get(): A {
+//     return 42 as A;
+//   }
+// }
+//
+// new Foo(Type<Person>(), 42).get().name
+//
+//
+// Model.define<Person>('persons').key<Key<Person>>(['id', 'age'])
