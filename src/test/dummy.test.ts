37b95d534769587fe69da8dceef107e010c8d09e
Henrik Hall <henrik.hall@sony.com>
2023-04-26 08:31:50 +0200

    test

--- src/test/dummy.test.ts
+++ src/test/dummy.test.ts
@@ -1,6 +1,6 @@
 import {randomBytes} from 'crypto';
 
-import DynamoClient from '../';
+import DynamoClient, {isDynamoError} from '../';
 
 type Person = {
   id: string;
@@ -36,4 +36,26 @@ describe('Models', () => {
     //   })
     // });
   });
+
+  it('should identify errors', async () => {
+    const client = new DynamoClient();
+
+    class PersonModel extends DynamoClient.model<Person>()
+        .withKey('id')
+        .withIndex('name-age-index', 'name', 'age')
+        .withCreator(x => ({
+          id: randomBytes(16).toString('hex'),
+          createdTime: now(),
+          modifiedTime: now()
+        }))
+        .withUpdater(x => ({modifiedTime: now()}))
+        .withTrigger((item, command, model) => console.log(`Trigger: ${model.name}.${command}: ${JSON.stringify(item)}`))
+        .class() {}
+
+    const persons = new PersonModel({client, name: 'persons'});
+    const error = await persons.get({key: {id: '42'}}).catch(err => err);
+    expect(error).toBeInstanceOf(Error);
+    expect(isDynamoError(error, 'RequestLimitExceeded')).toBeFalsy();
+    expect(isDynamoError(error, 'ResourceNotFound')).toBeTruthy();
+  })
 });
